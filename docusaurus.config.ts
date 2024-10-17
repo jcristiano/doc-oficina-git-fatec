4203c7b69e7be4a13773bf63a5227a8d1a44ff33
Cristiano <jcristaino.c@gmail.com>
2024-10-16 21:40:25 -0300

    ci: github actions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,17 +8,17 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jcristiano.github.io', 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/doc-oficina-git-fatec/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jcristiano', // Usually your GitHub org/user name.
   projectName: 'doc-oficina-git-fatec', // Usually your repo name.
 
-  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
